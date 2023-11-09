b0c218c52fad427ebd960de8fa5ffd210df9a804
Linus <linusong22@gmail.com>
2023-11-09 00:35:15 +0000

    modified v2, and cleaned up non-needed files

--- Project V2.py
+++ Project V2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import os
 
 class Vehicle:
     def __init__ (self, length = 1, target_velocity = 4, max_velocity = 5, 
@@ -37,6 +38,14 @@ class Vehicle:
         # this can be implemented with a recklessness prbability maybe(?)
         pass
     
+    def obstacle(self):
+        #TODO: collision function that simulates accident/obstacle that cars will lane switch to manouver around this obstacle
+        #obstacle present in a certain number of time steps, then after the time step is removed, how long will it take for the flow to be recovered?
+        pass
+
+
+    #TODO: capture speed at a certain point. there is a backward wave that makes cars move at a certain speed. can we label a car and measure it's velocity? 
+    # yes - there is VechicleID output, just add velocity output for the vehicle.
 
 class Traffic_Light:
     def __init__ (self, color):
@@ -65,11 +74,15 @@ class Road: #do i NEED this road class, or can this just be in the simulation cl
         self.number = int(density * length)
         self.speed_limit = speed_limit 
         self.bend = bend
-        self.connection_point = None
+        
+'''
+    def bend(angle, entrance_length, exit_length):
+        if self.bend = True:
+            angle = 
 
-    def connection(self, connection_point):
-        if 0 <= connection_point < self.length:
-            self.connection_point = connection_point
+            #TODO: Add "bend" fucntion to Road class to simulate bends
+
+'''
 
 class Network: 
     #TODO: add connection function that joins roads together to form a network.
@@ -83,14 +96,18 @@ class Network:
         #since the road generation is quite iffy. could set the joint at the end of the road? 
         #road generation is not a np.zeros array, so specifying a joint will be hard.
         pass
-
 class Simulation:
-    def __init__(self):
+    def __init__(self, save = False, output_dir = None):
         self.Road = Road()
         self.Vehicle = Vehicle()
         self.velocities = None
         self.positions = None
         self.data = []
+        self.output_dir = output_dir
+
+        if save:
+            if not os.path.exists(self.output_dir):
+                os.makedirs(self.output_dir)
 
     def initialize(self):
         if self.Road.density > 1.0:
@@ -122,39 +139,40 @@ class Simulation:
             raise Exception("Please call initialize() before update()")
         
         else:
+
             for _ in range(steps):
                 new_velocities = []
-                for road in self.network.roads:
+                for i in range(len(self.positions)):
+                    velocity = self.velocities[i]
+                    headway = (self.positions[(i + 1) % len(self.positions)] - self.positions[i] - 1) % self.Road.length
 
-                    for i in range(len(self.positions)):
-                        velocity = self.velocities[i]
-                        headway = (self.positions[(i + 1) % len(self.positions)] - self.positions[i] - 1) % self.Road.length
+                    if self.Vehicle.kindness == True:
+                        kindness = 1 + np.random.random()
+                        headway = headway * kindness
 
-                        if self.Vehicle.kindness == True:
-                            kindness = 1 + np.random.random()
-                            headway = headway * kindness
+                    if self.Vehicle.reckless == True:
+                        reckless = np.random.random()
+                        headway = headway * reckless
 
-                        if self.Vehicle.reckless == True:
-                            reckless = np.random.random()
-                            headway = headway * reckless
+                    velocity = min(velocity + 1, self.Vehicle.max_velocity)
+                    velocity = min(velocity, headway)
 
-                        velocity = min(velocity + 1, self.Vehicle.max_velocity)
-                        velocity = min(velocity, headway)
+                    if velocity > 0 and random.random() < self.Vehicle.slow_prob:
+                        velocity = max(velocity - 1, 0)
 
-                        if velocity > 0 and random.random() < self.Vehicle.slow_prob:
-                            velocity = max(velocity - 1, 0)
+                    print("velocity and position at time step(", _ ,"):", velocity)
+                    new_velocities.append(velocity)
+                    
 
-                        new_velocities.append(velocity)
+                self.velocities.append(new_velocities)
+                new_positions = [(pos + vel) % self.Road.length for pos, vel in zip(self.positions, self.velocities)]
+                self.positions = new_positions
+                self.data.append(self.positions[:])
 
-                    self.velocities = new_velocities
-                    new_positions = [(pos + vel) % self.Road.length for pos, vel in zip(self.positions, self.velocities)]
-                    self.positions = new_positions
-                    self.data.append(self.positions[:])
-
-
-        print(self.data)
-
-        return self.data
+        print("data: ", self.data)
+        print("velocities: ", self.velocities)
+        
+        return self.data, self.velocities, self.positions
 
     def flow_rate_ref_point(self, time_interval, reference_point=0):
         num_vehicles_passed = 0
@@ -181,15 +199,24 @@ class Simulation:
         # Calculate the total number of loops
         total_loops += (num_vehicles_passed / len(self.data))
 
-        print("Total Loops: %f" % total_loops)
-        print("Num Vehicles Passed: %d" % num_vehicles_passed)
+        #print("Total Loops: %f" % total_loops)
+        #print("Num Vehicles Passed: %d" % num_vehicles_passed)
         
         flow_rate = (num_vehicles_passed / len(self.data)) * (1 / time_interval)
-        print('Flow rate = %f' % flow_rate)
+        #print('Flow rate = %f' % flow_rate)
 
         return flow_rate
     
     def flow_rate_loop(self, time_interval):
+        """
+        flow rate loop counter
+
+        Args:
+            time_interval (int): time step
+
+        Returns:
+            flow_rate(float): flow rate at the end of the road
+        """
         num_passes = 0
         print(self.Vehicle.max_velocity)
 
@@ -221,7 +248,17 @@ class Simulation:
         return flow_rate
 
             
-    def plot_timespace(self, steps, plot = True):
+    def plot_timespace(self, steps, plot = True, save = False, folder = None, number = None):
+        """
+        plots time space diagram using data from self.data
+
+        Args:
+            steps (int): time step
+            plot (bool, optional): If True, shows plot of graph. Defaults to True.
+            save (bool, optional): If True, will save to file. Defaults to False.
+            folder (_type_, optional): Save location - required if save is True. Defaults to None.
+            number (_type_, optional): Used for keeping track of plots when iterating. Defaults to None.
+        """
         if plot == True:
             print('Simulation Complete. Plotting graph...')
             time_steps = range(steps)
@@ -232,60 +269,115 @@ class Simulation:
                 print('Vehicle ID: %s' %i)
                 new_data = [item[i] for item in self.data]
                 print('Position List: %s' %new_data)
-                plt.plot(new_data, time_steps, '-')
+                plt.plot(new_data, time_steps, '.', markersize = 0.5, color = 'gray')
 
+            plt.gca().xaxis.set_ticks_position('top')    
+            plt.gca().invert_yaxis()
             plt.title('Time Space diagram')
-            plt.xlabel('Vechicle Position')
+            plt.xlabel('Vehicle Position')
             plt.ylabel('Time')
-            plt.show()
+            plt.figtext(0.1, 0.005, f'Density = {self.Road.density}, Slow Prob = {self.Vehicle.slow_prob}, Max velocity = {self.Vehicle.max_velocity}', fontsize= 9, color='black')
+
+            if save:
+                self.output_dir = os.path.join(folder, f'Time Space Plot {number}.png')
+                fig = plt.gcf()
+                fig.set_size_inches(12,12)
+                plt.savefig(self.output_dir, dpi = 100)
+                plt.clf()
+
+            else:
+                plt.show()
 
         else:
             print('Simulation Complete. Set plot = True to see the plot.')
         
-    def plot_density(self, steps, plot=True, isAvg = True):
+    def plot_velocity(self, steps, plot=True, save=False, folder=None):
+
+        if plot:
+            time_steps = range(steps)
+
+            for i in range(self.Road.number):
+                print("Vehicle ID: %s" % i)
+                velocity_data = self.velocities  # Use self.velocities directly
+                print("Velocity List: %s" % velocity_data)
+                plt.plot(velocity_data, time_steps, '.', markersize=1)
+
+            plt.gca().xaxis.set_ticks_position('top')
+            plt.gca().invert_yaxis()
+            plt.title("Velocity - Time diagram")
+            plt.xlabel("Vehicle Velocity")
+            plt.ylabel("Time")
+            plt.figtext(0.1, 0.005, f'Density = {self.Road.density}, Slow Prob = {self.Vehicle.slow_prob}, Max velocity = {self.Vehicle.max_velocity}', fontsize=9, color='black')
+            plt.show()
+
+
+            
+    def plot_density(self, steps, plot=True, isAvg = True, save = False, folder = None, number = None):
         densities = []
+        flow_rate = []
         flow_rates = []
         flow_rate_avgs = []
         
         if plot:
             if isAvg:
-                for i in range(1, 999):
-                    p = 0.001*i
+                for i in range(10, 250):
+                    p = 0.004*i
                     print('density: %f' % p)
                     densities.append(p)
+                    sim = Simulation()
+                    sim.Road = Road(density= p)                                  
+                    sim.initialize()
+                    sim.update(steps)
+                    flow_rate.append(sim.flow_rate_loop(steps))
 
-                    for j in range(30):  
-                        sim = Simulation()
-                        sim.Road = Road(density= p)                                  
-                        sim.initialize()
-                        sim.update(steps)
+                    #for j in range(10):  
+                        #sim = Simulation()
+                        #sim.Road = Road(density= p)                                  
+                        #sim.initialize()
+                        #sim.update(steps)
                         #print(sim.flow_rate(steps))
-                        flow_rates.append(sim.flow_rate_loop(steps))       
+                        #flow_rates.append(sim.flow_rate_loop(steps))       
 
-                    flow_rate_avg = sum(flow_rates)/30
-                    flow_rate_avgs.append(flow_rate_avg)
-                    print(flow_rate_avgs)
+                #flow_rate_avg = sum(flow_rates)/10
+                #flow_rate_avgs.append(flow_rate_avg)
+                #print(flow_rate_avgs)
+                #print(np.shape(flow_rate_avgs))
 
                 #print("Density:" + str(densities))
                 #print(np.shape(densities))
                 #print("Flow Rate:" + str(flow_rate_avg))
                 #print(np.shape(flow_rate_avg))
-                plt.plot(densities, flow_rate_avgs, linestyle = '-')
+                plt.plot(densities, flow_rate, linestyle = '-')
                 plt.title('Average Flow Density Relationship')
                 plt.xlabel("Density")
                 plt.ylabel("Flow rate")
-                plt.show()
+                plt.figtext(0.1, 0.005, f'Max velocity = {self.Vehicle.max_velocity}, Slow Prob = {self.Vehicle.slow_prob}', fontsize= 9, color = 'black')
+
+                if save:
+                    self.output_dir = os.path.join(folder, f'Flow Density {number}.png')
+                    plt.savefig(self.output_dir)
+                    plt.clf()
+
+                else:
+                    plt.show()
 
 
-random.seed(200)
-steps = 100
-sim = Simulation()
-sim.Vehicle = Vehicle(max_velocity= 5, slow_prob= 0.5, kindness = True)
-sim.Road = Road(length= 1000, density=10/1000)
-sim.initialize()
-sim.update(steps)
-sim.flow_rate_loop(steps)
-sim.plot_timespace(steps)
-sim.plot_density(steps, isAvg = True)
+        
+debug = True
+
+if debug:
+    steps = 100
+    seeds = 100
+    random.seed(seeds)
+    sim = Simulation()
+    sim.Vehicle = Vehicle(max_velocity = 5, slow_prob = 0.5)
+    sim.Road = Road(length= 100, density=2/100)
+    sim.initialize()
+    sim.update(steps)
+    sim.flow_rate_loop(steps)
+    sim.plot_timespace(steps)
+    sim.plot_velocity(steps)
+    sim.plot_density(steps, isAvg = True)
+
 
 
